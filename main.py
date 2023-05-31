e3c794a1cd00f5b63b9a669dc32891bfe5fcb99c
VPesherov <vyacheslav.pesherov@yandex.ru>
2023-05-31 21:42:11 +0700

    modified main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-import requests
-
+from VkClass import VkApiHandler
+import os
+from dotenv import load_dotenv
+from pprint import pprint
 
 def main():
-    pass
+    load_dotenv()
+    vk_api_token = os.getenv('VK_API_TOKEN')
+    vk_api_version = os.getenv('VERSION')
+    user1 = VkApiHandler(vk_api_token, vk_api_version)
+    data = user1.get_all_user_photos('1')
+    pprint(data)
 
 
 if __name__ == '__main__':
