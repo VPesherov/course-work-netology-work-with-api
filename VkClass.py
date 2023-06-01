d9f543bff8eefde40f7bb0e7ff843f6dc5a79f08
VPesherov <vyacheslav.pesherov@yandex.ru>
2023-06-01 21:35:18 +0700

    completed VkClass

--- VkClass.py
+++ VkClass.py
@@ -1,5 +1,5 @@
 import requests
-
+from pprint import pprint
 
 class VkApiHandler:
     base_url = 'https://api.vk.com/method/'
@@ -9,6 +9,26 @@ class VkApiHandler:
             'access_token': access_token,
             'v': version
         }
+
+    def get_photos_with_max_size(self, photos):
+        # url : (likes, data)
+        photos_dict = {}
+        photo_types = 'wzyrqpoxms'
+        for photo in photos['response']['items']:
+            date = photo['date']
+            flag = False
+            for photo_type in photo_types:
+                # print(photo_type, end="")
+                for size in photo["sizes"]:
+                    size_photo = size['type']
+                    if size_photo == photo_type:
+                        url = size['url']
+                        flag = True
+                        break
+                if flag is True:
+                    break
+            photos_dict[url] = (photo['likes']['count'], date)
+        return photos_dict
 
     def get_all_user_photos(self, owner_id, album_id='profile', extended='1'):
         method_name = f'{self.base_url}photos.get'
